9ffc77bf37069aa9a0b67410eb5327d82c02c333
thanksstevenkim <stevenkim0325@gmail.com>
2024-10-10 22:01:26 +0900

    Fixed Issues

--- app/javascript/mastodon/store/middlewares/loading_bar.ts
+++ app/javascript/mastodon/store/middlewares/loading_bar.ts
@@ -76,9 +76,9 @@ export const loadingBarMiddleware = (
       }
 
       if (isPending) {
-        dispatch(showLoading());
+        dispatch(showLoading() as UnknownAction);
       } else if (isFulfilled || isRejected) {
-        dispatch(hideLoading());
+        dispatch(hideLoading() as UnknownAction);
       }
 
       return next(action);
