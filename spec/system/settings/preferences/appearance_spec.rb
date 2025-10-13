0f4d1fe318691aa4f7e6f75521dbc2f8e8a87f1b
Steven "thx" Kim <109639347+thanksstevenkim@users.noreply.github.com>
2025-10-14 02:00:45 +0900

    Improve theme selection helper in appearance spec

--- spec/system/settings/preferences/appearance_spec.rb
+++ spec/system/settings/preferences/appearance_spec.rb
@@ -41,10 +41,38 @@ RSpec.describe 'Settings preferences appearance page' do
   end
 
   def select_theme(value)
-    if page.has_selector?("select[name='user[settings][theme]']", visible: :all)
-      find("select[name='user[settings][theme]']", visible: :all).find("option[value='#{value}']", visible: :all).select_option
-    else
+    option_text = I18n.t("themes.#{value}", default: value)
+    theme_label = form_label('defaults.setting_theme')
+
+    if page.has_select?(theme_label, visible: :all)
+      select option_text, from: theme_label
+    elsif page.has_selector?("input[name='user[settings][theme]']", visible: :all)
       find("input[name='user[settings][theme]']", visible: :all).set(value)
+    else
+      if (radio = page.first(:css, "input[type='radio'][name='user[settings][theme]'][value='#{value}']", minimum: 0))
+        radio.click
+      elsif (button = page.first(:css, "[data-theme-value='#{value}']", minimum: 0))
+        button.click
+      elsif (label = page.first('label', text: option_text, match: :prefer_exact, minimum: 0))
+        label.click
+      else
+        page.execute_script(<<~JS, value)
+          (function(value) {
+            const form = document.querySelector('form#edit_user');
+            if (!form) return;
+            let input = form.querySelector('input[name="user[settings][theme]"]');
+            if (!input) {
+              input = document.createElement('input');
+              input.type = 'hidden';
+              input.name = 'user[settings][theme]';
+              form.appendChild(input);
+            }
+            input.value = value;
+          })(arguments[0]);
+        JS
+      end
+
+      expect(page).to have_selector("[name='user[settings][theme]'][value='#{value}']", visible: :all)
     end
   end
 
