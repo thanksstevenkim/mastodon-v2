cedc50d0184e4a5e4fb02cac572541314cca580b
thanksstevenkim <stevenkim0325@gmail.com>
2026-06-03 12:29:58 +0000

    update lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 const config = {
   '*': 'prettier --ignore-unknown --write',
-  'Gemfile|*.{rb,ruby,ru,rake}': 'bin/rubocop --force-exclusion -a',
+  'Gemfile|*.{rb,ruby,ru,rake}':
+    'bundle _2.7.2_ exec rubocop --force-exclusion -a',
   '*.{js,jsx,ts,tsx}': 'eslint --fix',
   '*.{css,scss}': 'stylelint --fix',
   '*.haml': 'bin/haml-lint -a',
