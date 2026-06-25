e2eca616eb233c0340d4bd3acc4c11337fc4ba66
thanksstevenkim <stevenkim0325@gmail.com>
2026-06-25 09:40:10 +0000

    Fix duplicate SoundIcon import

--- app/javascript/mastodon/features/compose/components/upload.tsx
+++ app/javascript/mastodon/features/compose/components/upload.tsx
@@ -10,7 +10,6 @@ import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 
 import CloseIcon from '@/material-icons/400-20px/close.svg?react';
-import SoundIcon from '@/material-icons/400-24px/audio.svg?react';
 import EditIcon from '@/material-icons/400-24px/edit.svg?react';
 import SoundIcon from '@/material-icons/400-24px/graphic_eq.svg?react';
 import WarningIcon from '@/material-icons/400-24px/warning.svg?react';
