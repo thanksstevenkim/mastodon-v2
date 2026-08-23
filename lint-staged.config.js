17ec0b7cc536a118ed1d3320f115ddfc1bb6ec7a
thanksstevenkim <stevenkim0325@gmail.com>
2026-08-23 10:20:46 +0000

    Fix local pre-commit lint execution

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,7 +4,7 @@ const config = {
     'rbenv exec bundle exec rubocop --force-exclusion -a',
   '*.{js,jsx,ts,tsx}': 'eslint --fix',
   '*.{css,scss}': 'stylelint --fix',
-  '*.haml': 'bin/haml-lint -a',
+  '*.haml': 'rbenv exec bundle exec haml-lint -a',
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
   'app/javascript/**/*.{js,jsx,ts,tsx}': () => [
     `yarn i18n:extract`,
